8d38708d3bea39da7c7662d612c8eee588fad865
qin yuanhang <19376479@buaa.edu.cn>
2022-08-01 10:43:03 +0800

    整合

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
 export default {
   name: 'App',
   components: {
-
   }
 }
 </script>
@@ -46,41 +45,4 @@ ul
   background-color: transparent;
   border-radius: 32px;
 }
-<<<<<<< HEAD
-body
-  {
-    margin: 0;
-    padding: 0;
-  }
-  #app 
-  {
-    font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
-    'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-  }
-  a{text-decoration: none;}
-  ul
-  {
-    padding-inline-start: 0 !important;
-    list-style-type: none;
-  }
-  ::-webkit-scrollbar 
-  {
-    width: 10px;
-    height: 10px;
-  }
-  ::-webkit-scrollbar-thumb
-  {
-    background-color: rgb(167, 159, 159);
-    border-radius: 32px;
-  }
-  ::-webkit-scrollbar-track 
-  {
-    background-color: transparent;
-    border-radius: 32px;
-  }
-=======
->>>>>>> e0facdf7a05ae5a022734d5b90216e902172d308
 </style>
